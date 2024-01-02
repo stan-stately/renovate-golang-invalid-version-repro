17d688d072d703ceb0ce8addc0d1f158fb04751c
Stan Tsouvallas <stan@stately.cloud>
2024-01-02 15:12:40 +1100

    WIP

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/Masterminds/sprig/v3"
 	"golang.org/x/exp/maps"
 )
 
 func main() {
 	maps.Keys(map[string]string{"hello": "world"})
+	sprig.GenericFuncMap()
 }
